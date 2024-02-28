88ae3375077b61a042f196ad1224922345f007b8
Emerald Zhu <zhuemerald@gmail.com>
2024-02-29 01:02:38 +0300

    add router links

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
-<script setup>
-
-</script>
-
 <template>
   <div id="app">
-    <h1>Welcome to Your Vue App</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
   </div>
 </template>
 
-<style scoped>
+<script setup>
+</script>
 
+<style>
+/* Global styles */
 </style>
